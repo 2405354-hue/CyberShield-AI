b3ea49035f117d77116a9c69460a07156c203659
Kamal Tripathy <2405354@kiit.ac.in>
2026-07-26 17:37:56 +0530

    Added extract packet features
    - Extracted Source IP
    - Extracted Destination IP
    - Extracted Protocol
    - Extracted Source and Destination Ports
    - Extracted Packet Length

--- packet_capture/capture.py
+++ packet_capture/capture.py
@@ -1,9 +1,58 @@
-from scapy.all import sniff
+from scapy.all import *
+
+protocols = {
+    1: "ICMP",
+    2: "IGMP",
+    4: "IPIP",
+    6: "TCP",
+    8: "EGP",
+    17: "UDP",
+    41: "IPv6",
+    43: "IPv6-Route",
+    44: "IPv6-Frag",
+    46: "RSVP",
+    47: "GRE",
+    50: "ESP",
+    51: "AH",
+    58: "ICMPv6",
+    59: "No Next Header",
+    60: "IPv6-Opts",
+    88: "EIGRP",
+    89: "OSPF",
+    94: "IPIP",
+    103: "PIM",
+    112: "VRRP",
+    115: "L2TP",
+    132: "SCTP",
+    136: "UDP-Lite",
+    137: "MPLS",
+    139: "HIP",
+    140: "Shim6"
+}
 
 def packet_callback(packet):
-    print(packet.summary())
 
-print("Starting packet capturing...")
+    if IP in packet:
+
+        print("Source IP           :",packet[IP].src)
+        print("Destination IP      :",packet[IP].dst)
+
+        protocol = protocols.get(packet[IP].proto, f"Unknown ({packet[IP].proto})")
+        print("Protocol            :", protocol)
+
+        if TCP in packet:
+
+            print("Source Port         :", packet[TCP].sport)
+            print("Destination Port    :", packet[TCP].dport)
+
+        elif UDP in packet:
+
+            print("Source Port         :", packet[UDP].sport)
+            print("Destination Port    :", packet[UDP].dport)
+
+        print("Packet length       :", len(packet), "bytes")
+        print("_ "*50)
+        
 print("Capturing 5 packets...\n")
 
 sniff(count=5, prn=packet_callback)
