c627b7b6925cc6c121ed8dde7a34d85630231616
Kamal Tripathy <2405354@kiit.ac.in>
2026-08-03 16:30:49 +0530

    Load and prepare CICIDS2017 dataset
    - Added CICIDS2017 cleaned dataset to the data folder.
    - Loaded the dataset using Pandas.
    - Explored dataset structure and columns.
    - Separated features (X) and target (Attack Type).
    - Split the dataset into training and testing sets using train-test-split().
    - Prepared the project for Random Forest model training.

--- models/ml.py
+++ models/ml.py
@@ -1,24 +1,42 @@
 import pandas as pd
 
-data = pd.read_csv("../captured_packets.csv")
+from sklearn.model_selection import train_test_split
 
-features = data[
-    [
-        "Ethernet Type",
-        "Direction",
-        "TTL",
-        "IP ID",
-        "Fragment Offset",
-        "Protocol",
-        "Source Port",
-        "Destination Port",
-        "Service",
-        "TCP Flags",
-        "Packet Length"
-    ]
-]
+data = pd.read_csv("../data/cicids2017_cleaned.csv")
 
-print(features.head())
-print(features.shape)
-print(features.columns)
-print(data.info())
+#print(data.head())
+
+print(data.shape)
+
+#print(data.columns)
+
+#print(data.info())
+
+X = data.drop("Attack Type", axis=1)
+
+y = data["Attack Type"]
+
+print(X.head())
+
+print(y.head())
+
+print("Features Shape :", X.shape)
+
+print("Target Shape   :", y.shape)
+
+print("\nAttack Types:")
+
+print(y.unique())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("Training Features :", X_train.shape)
+print("Testing Features  :", X_test.shape)
+
+print("Training Labels   :", y_train.shape)
+print("Testing Labels    :", y_test.shape)
