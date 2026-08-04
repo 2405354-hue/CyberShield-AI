447de3fb034678fbf01ce7b4995cf4beb6e4f217
Kamal Tripathy <2405354@kiit.ac.in>
2026-08-04 22:14:15 +0530

    Train and evaluate Random Forest model
    - Trained a Random Forest classifier using the CICIDS2017 dataset.
    - Generated predictions on the testing dataset.
    - Calculated model accuracy.
    - Added confusion matrix for prediction analysis.
    - Generated a classification report with precision, recall, and F1-score.

--- models/ml.py
+++ models/ml.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
+import time
+
 from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import (accuracy_score, confusion_matrix , classification_report)
 
 data = pd.read_csv("../data/cicids2017_cleaned.csv")
 
@@ -24,9 +30,16 @@ print("Features Shape :", X.shape)
 
 print("Target Shape   :", y.shape)
 
+sample = data.sample(n=100000, random_state=42)
+
+X = sample.drop("Attack Type", axis=1)
+y = sample["Attack Type"]
+
 print("\nAttack Types:")
 
 print(y.unique())
+
+start = time.time()
 
 X_train, X_test, y_train, y_test = train_test_split(
     X,
@@ -35,8 +48,39 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
+end = time.time()
+
+print("Model trained successfully!")
+print(f"Training Time: {end-start:.2f} seconds")
+
 print("Training Features :", X_train.shape)
 print("Testing Features  :", X_test.shape)
 
 print("Training Labels   :", y_train.shape)
 print("Testing Labels    :", y_test.shape)
+
+model = RandomForestClassifier(n_estimators=20,random_state=42,n_jobs=-1)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Accuracy: {accuracy*100:.2f}%")
+
+cm = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix:")
+print(cm)
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+print("First 10 Predictions:")
+print(y_pred[:10])
+
+print("Model trained successfully!")
+
+print(X.dtypes.unique())
