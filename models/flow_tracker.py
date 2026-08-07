88be2c377bfe949b769e8d2e2d7cc36f37b9f4b7
Kamal Tripathy <2405354@kiit.ac.in>
2026-08-07 07:44:01 +0530

    Implement basic network flow tracker
    - Read captured packet data using Pandas.
    - Processed packets one by one.
    - Generated flow keys using the network 5-tuple.
    - Tracked packet count and total bytes for each flow.
    - Printed detected flow statistics.
    - Built the first working version of the flow tracker.

--- models/flow_tracker.py
+++ models/flow_tracker.py
@@ -1,20 +1,41 @@
+import pandas as pd
+
 flows = {}
 
-print("Flow tracker created successfully!")
+packets = pd.read_csv("../captured_packets.csv")
 
-flow_key = (
-    source_ip,
-    destination_ip,
-    source_port,
-    destination_port,
-    protocol
-)
+print(packets.head())
 
-if flow_key not in flows:
-    flows[flow_key] = {
-        "packet_count": 1,
-        "total_bytes": packet_length
-    }
-else:
-    flows[flow_key]["packet_count"] += 1
-    flows[flow_key]["total_bytes"] += packet_length
+for index, packet in packets.iterrows():
+    source_ip = packet["Source IP"]
+    destination_ip = packet["Destination IP"]
+    source_port = packet["Source Port"]
+    destination_port = packet["Destination Port"]
+    protocol = packet["Protocol"]
+    packet_length = packet["Packet Length"]#used to claculate the stastics of flows
+
+    flow_key = (
+        source_ip,
+        destination_ip,
+        source_port,
+        destination_port,
+        protocol
+    )
+
+    if flow_key not in flows:
+        flows[flow_key] = {
+            "packet_count": 1,
+            "total_bytes": packet_length
+        }
+    else:
+        flows[flow_key]["packet_count"] += 1
+        flows[flow_key]["total_bytes"] += packet_length
+
+print("\nDetected Flows:\n")
+
+for key, value in flows.items():
+
+    print("Flow           :", key)
+    print("Packets        :", value["packet_count"])
+    print("Total Bytes    :", value["total_bytes"])
+    print("-" * 50)
